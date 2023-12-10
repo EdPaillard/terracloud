872660d05dd01cee36aa84711d8409834df0e4b6
Théo NOL-YGONNIN <theoygo20@outlook.fr>
2023-12-10 17:54:20 +0100

    update file database.php

--- config/database.php
+++ config/database.php
@@ -45,13 +45,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => getenv('DATABASE_URL'),
-            'host' => getenv('DB_HOST', '127.0.0.1'),
-            'port' => getenv('DB_PORT', '3306'),
-            'database' => getenv('DB_DATABASE', 'forge'),
-            'username' => getenv('DB_USERNAME', 'forge'),
-            'password' => getenv('DB_PASSWORD', ''),
-            'unix_socket' => getenv('DB_SOCKET', ''),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -60,7 +60,7 @@ return [
             'engine' => null,
             'sslmode' => 'require',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => getenv('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
